f45dc22681db1a37e2dbb35f0eb6c0e3484a74c7
Taewoo Kim <kr26790@gmail.com>
2022-12-11 00:05:31 +0900

    Add File "discord_bot_sender", and finished check discord image send. Now, We have to develop Saves image when face is detected, and send it to discord.

---
+++ discord_bot_sender.py
@@ -0,0 +1,30 @@
+import token_discord
+import discord
+from discord.ext import commands
+
+intents=discord.Intents.all()
+intents.message_content=True
+intents.members=True
+intents.typing=True
+intents.presences=True
+
+app=commands.Bot(command_prefix='!',intents=discord.Intents.all())
+
+@app.event
+async def on_ready():
+    print('Done')
+    await app.change_presence(status=discord.Status.online,activity=None)
+
+@app.command()
+async def hello(ctx):
+    await ctx.send('Hello I am a Bot!')
+
+@app.command()
+async def pic(ctx):
+    with open('./cache.jpg','rb') as f:
+        pic=discord.File(f)
+        await ctx.send(file=pic)
+        await ctx.send('File send check')
+
+print(token_discord.token_dis)
+app.run(token_discord.token_dis)
